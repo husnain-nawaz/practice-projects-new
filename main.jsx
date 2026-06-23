63d8410b595807f2df5fea988f435e2e23d63389
husnain-nawaz <64336728+husnain-nawaz@users.noreply.github.com>
2026-06-23 23:51:53 +0500

    Add files via upload
    .

--- main.jsx
+++ main.jsx
@@ -240,23 +240,202 @@ import App from './App.jsx'
 
 
 
+// function Myform(){
+//     const [inputs, setInputs] = useState({
+//         firstname: "",
+//         tomato: false,        
+//         onion: false,
+//     });
+//     const handlechange = (e)=>{
+//         const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+//         const name = e.target.name;
+//         setInputs(values => ({...values, [name]: value}))
+//     }
+//     const handleSubmit = (event) => {
+//         let filling = '';
+//         if(inputs.tomato) filling+='tomato';
+//         if(inputs.onion){
+//             if(inputs.tomato) filling += " and ";
+//             filling += "onion";
+//         }
+//         if(filling == "") filling = "no filling";
+//         alert(`${inputs.firstname} wants a burger with ${filling}`)
+//         event.preventDefault();
+//     };
+//     return(
+//         <form onSubmit = {handleSubmit}>
+//             <label>
+//                 My name is:
+//                 <input type='text' name='firstname' value={inputs.firstname} onChange={handlechange}></input>
+//             </label>
+//             <p>I want a burger with: </p>
+//             <label>
+//                 Tomato:
+//                 <input type="checkbox" name='tomato' checked={inputs.tomato} onChange={handlechange} />
+//             </label>
+//             <label>
+//                 Onion:
+//                 <input type="checkbox" name='onion' checked = {inputs.onion} onChange={handlechange} />
+//             </label>
+//             <button type='submit' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+//         </form>
+//     )
+// }
+
+// createRoot(document.getElementById('root')).render(
+//     <Myform/>
+// )
+
+
+
+// function Myform(){
+//     const [inputs,setInputs] = useState({
+//         firstname: "",
+//         tomato : false,
+//         onion : false, // true karny sy bluetick lag jaye ga
+//     })
+//     const handlechange = (e) => {
+//         const value = e.target.type === "checkbox" ?  e.target.checked : e.target.value;
+//         const name = e.target.name;
+//         setInputs(values => ({...values, [name]: value}));
+//     }
+//     const handlesubmit = (event) => {
+//         let filling = "";
+//         if(inputs.tomato) filling+="tomato"; 
+//         if(inputs.onion){
+//             if(inputs.tomato) filling += " add ";
+//             filling += "onion"
+//         }
+//         if(filling === "") filling="no filling";
+//         alert(`${inputs.firstname} wanst a burger with ${filling}`)
+//         event.preventDefault();
+//     }
+//     return (
+//         <form onSubmit={handlesubmit}>
+//             <label>
+//                 My name is:
+//                 <input type="text" name='firstname' value={inputs.firstname} onChange={handlechange} />
+//             </label>
+//             <p>I want a burger with: </p>
+//             <label>
+//                 Tomato:
+//                 <input type="checkbox" name='tomato' checked={inputs.tomato} onChange={handlechange} />
+//             </label>
+//             <label>
+//                 Onion:
+//                 <input type="checkbox" name="onion" checked={inputs.onion} onChange={handlechange} />
+//             </label>
+//             <button type='submit'>submit</button>
+//         </form>
+//     )
+// }
+// createRoot(document.getElementById('root')).render(
+//     <Myform/>
+// )
+
+
+
+// function Myform(){
+//     const [selectedfruit, setselectedfruit] = useState("mango");
+//     const handlechnage = (event) =>{
+//         setselectedfruit(event.target.value);
+//     }
+//     const handlesubmit = (event)=>{
+//         alert(`Your Favourite fruit is: ${selectedfruit}`);
+//         event.preventDefault();
+//     }
+//     return(
+//         <form onSubmit={handlesubmit}>
+//             <p>Select Your favourite fruit: </p>
+//             <label>
+//                 <input type="radio" name='fruit' value="apple" checked={selectedfruit==="apple"} onChange={handlechnage} />
+//                 Apple
+//             </label>
+//             <br />
+//             <label>
+//                 <input type="radio" name='fruit' value="banana" checked={selectedfruit==="banana"} onChange={handlechnage} />
+//                 banana
+//             </label>
+//             <br />
+//             <label>
+//                 <input type="radio" name='fruit' value="mango" checked={selectedfruit==="mango"} onChange={handlechnage} />
+//                 mango
+//             </label>        
+//             <br />
+//             <button type="submit">submit</button>
+//         </form>
+//     )
+// }
+
+// createRoot(document.getElementById('root')).render(
+//     <Myform/>
+// )
+
+
+
+// import { createPortal } from 'react-dom'
+
+// function mychild(){
+//     return createPortal(
+//         <div>
+//             Welcome
+//         </div>,
+//         document.body
+//     )
+// }
+
+
+// function App() {
+//   return (
+//     <div style={{
+//       width: "200px",
+//       height: "100px",
+//       overflow: "hidden",
+//       border: "2px solid black"
+//     }}>
+//       <h3>My App</h3>
+
+//       <div style={{
+//         width: "300px",
+//         height: "150px",
+//         background: "yellow"
+//       }}>
+//         Modal
+//       </div>
+//     </div>
+//   );
+// }
 
 
 
 
+function Model({isopen,onclose,children}){
+    if(!isopen){
+        return null;
+    }
+    else{
+    return(
+        <div>
+            {children}
+            <button onClick={onclose}>close</button>
+        </div>
+    )}
+}
+function MyApp(){
+    const [isopen, setisopen] = useState(false);
+    return(
+        <div>
+            <button onClick={()=>setisopen(true)}>OPEN</button>
+            <Model isopen={isopen} onclose={()=>setisopen(false)}>
+                <p>model content</p>
+            </Model>
+        </div>
+    )
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+createRoot(document.getElementById('root')).render(
+  <MyApp />
+)
 
 
 
